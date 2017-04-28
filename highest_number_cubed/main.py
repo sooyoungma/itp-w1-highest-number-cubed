636737ff572868f53031f0b091244334117c4f50
sooyoungma <sooyma@gmail.com>
2017-04-28 02:00:23 +0000

    we did it

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,12 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    current_number = 0
+
+    while True:
+        current_number += 1
+        if current_number ** 3 > limit:
+            return current_number - 1 
+    
+
+
